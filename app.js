98b561cf7481e3b9867e7a4952b8991cb80b4a47
maedson lima <maedsonrhcp@gmail.com>
2025-01-24 18:04:53 -0300

    função para automatizar adicionada, juntamente com o botão

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-// Para alterar algo do HTML usando o Java Script, usa o comando "document" 
-// pra o sistema saber qual parte do HTML, USAMOS ".querySelector"
-// colocamos depois qual elemento queremos alterar, no caso o "H1"
-// para inserir algo dentro da variável que selecionamos "titulo", USAMOS O .innerHTML
-// innerHTML significa dentro do html.
-
-let titulo = document.querySelector ('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
-
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10  ';
-
 
 /* function = um trecho de código que vai fazer uma função, vai verificar o chute, saber 
 se ele ta certo, etc.. por padrão ela deve ter apenas UMA responsabilidade por isso 
 escrevemos dentro dos { } assim como o while, nesse caso nossa função vai ser verificar o chute
 */
-function verificarChute() {
 
-let numero1 = parseInt (prompt("insira um número inteiro"));
-let numero2 = parseInt (prompt("insira outro número inteiro"))
-let soma = (numero1 + numero2);
-alert(`a soma  dos números é ${soma}`);
-
-
+function exibirTextoNaTela(tag, texto) {
+// Define uma função chamada "exibirTextoNaTela" que recebe dois parâmetros: 
+// "tag" (a tag HTML que será selecionada) e "texto" (o conteúdo que será exibido dentro da tag).
+    
+ let campo = document.querySelector(tag);
+// Usa o método `document.querySelector(tag)` para selecionar o elemento HTML correspondente ao parâmetro "tag".
+// O elemento encontrado é armazenado na variável `campo`.
+    
+campo.innerHTML = texto;
+// Altera o conteúdo interno do elemento selecionado (`campo`) para o valor fornecido no parâmetro "texto".
 }
 
+exibirTextoNaTela ('h1', 'Jogo do número secreto');
+// Chama a função "exibirTextoNaTela", passando 'h1' como o parâmetro "tag" e 'Jogo do Número Secreto' como o parâmetro "texto".
+// Isso altera o conteúdo do elemento `<h1>` na página para "Jogo do Número Secreto".
+
+exibirTextoNaTela ('p', 'Escolha um número entre 1 e 10');
+
+function verificarChute () {
+
+    console.log ('o botão foi clicado');
+}
