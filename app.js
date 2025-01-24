0e6e7cfb8f8b91ed4fabca566cafd29374adb504
maedson lima <maedsonrhcp@gmail.com>
2025-01-24 02:29:36 -0300

    Adicionado as funções e explicações do que e como usar

--- app.js
+++ app.js
@@ -10,3 +10,14 @@ titulo.innerHTML = 'Jogo do Número Secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10  ';
 
+
+/* function = um trecho de código que vai fazer uma função, vai verificar o chute, saber 
+se ele ta certo, etc.. por padrão ela deve ter apenas UMA responsabilidade por isso 
+escrevemos dentro dos { } assim como o while, nesse caso nossa função vai ser verificar o chute
+*/
+function verificarChute() {
+
+console.log ('botão foi clicado');
+
+}
+
