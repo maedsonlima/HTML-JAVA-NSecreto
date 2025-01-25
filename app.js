7139f4d2a7a294294f8d39667752aef79c77d5fb
maedson lima <maedsonrhcp@gmail.com>
2025-01-25 01:24:46 -0300

    Jogo Quase Funcionando, adicionado boleanas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+let numeroSecreto = gerarNumeroAleatorio ();
 
 /* function = um trecho de código que vai fazer uma função, vai verificar o chute, saber 
 se ele ta certo, etc.. por padrão ela deve ter apenas UMA responsabilidade por isso 
@@ -22,7 +23,22 @@ exibirTextoNaTela ('h1', 'Jogo do número secreto');
 
 exibirTextoNaTela ('p', 'Escolha um número entre 1 e 10');
 
+
+
 function verificarChute () {
 
-    console.log ('o botão foi clicado');
+let chute = document.querySelector('input').value;
+//Cria uma variável chamada `chute` que armazena o valor inserido pelo usuário em um campo `<input>` no HTML.
+    
+console.log(numeroSecreto);
+console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroAleatorio () {
+  return parseInt(Math.random() *10 + 1);
+
+ // Gera um número aleatório entre 1 e 10 (inclusivo) e retorna o valor.
+// Sem o return, a função executaria o cálculo, mas não devolveria o resultado.
+}
+
+
